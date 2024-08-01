7cb84ddb86ac43d099c95eb6fbbd3fafa9b82075
Anamika Sawhney <anamika.sawhney22@gmail.com>
2024-08-01 10:32:08 +0530

    Add project files.

---
+++ Assignment4/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    internal class Product
+    {
+        int pCode;
+        public string pName;
+        public int qtyInStock;
+        int discountAllowed;
+        static string brand;
+        public int price;
+        public int amount;
+        public void GetDetails()
+        {
+            Console.WriteLine("enter pCode");
+            pCode = byte.Parse(Console.ReadLine());
+            Console.WriteLine("enter pName");
+            pName = Console.ReadLine();
+            Console.WriteLine("enter qtyInStock");
+            qtyInStock = byte.Parse(Console.ReadLine());
+            Console.WriteLine("enter discountAllowed");
+            discountAllowed = byte.Parse(Console.ReadLine());
+            Console.WriteLine("enter price");
+            price = Int16.Parse(Console.ReadLine());
+
+        }
+        static Product()
+        {
+            brand = "Lewis";
+        }
+        public void DisplayDetails()
+        {
+            Console.Write("pCode " + pCode);
+            Console.Write("\tproduct name " + pName);
+            Console.Write("\tqty in stock is " + qtyInStock);
+            Console.Write("\tdicsount allowed " + discountAllowed);
+            Console.Write("\tprice " + price);
+            Console.WriteLine("amount " + amount);
+
+        }
+        public static void DisplayBrand()
+        {
+            Console.WriteLine("Brand is " + brand);
+        }
+    }
+}
---
+++ Assignment4/Program.cs
@@ -0,0 +1,130 @@
+﻿namespace Assignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("THIS IS FOR BRAND ");
+            Product.DisplayBrand();
+            List<Product> products = new List<Product>();
+            List<Product> cart = new List<Product>();
+            string choice = "y";
+            while (choice.Equals("y"))
+            {
+                Console.WriteLine("WHo are you?" +
+                    "\nEnter 1 for Admin\nENter 2 for Customer");
+                int ch = byte.Parse(Console.ReadLine());
+                switch (ch)
+                {
+                    case 1:
+
+                        {
+                            int c = AdminMenu();
+                            switch (c)
+                            {
+                                case 1:
+                                    {
+                                        string repeat = "y";
+                                        while (repeat.Equals("y"))
+                                        {
+                                            Product product = new Product();
+                                            product.GetDetails();
+                                            products.Add(product);
+                                            Console.WriteLine("add more product");
+                                            repeat = Console.ReadLine();
+                                        }
+                                        break;
+                                    }
+
+                                case 2:
+                                    {
+                                        foreach (Product product in products)
+                                        {
+                                            product.DisplayDetails();
+
+                                        }
+                                        break;
+                                    }
+
+
+                            }
+                            break;
+
+                        }
+                    case 2:
+                        {
+                            int c = CustomerMenu();
+                            switch(c)
+                            {
+
+                                case 1:
+                                    {
+                                        string repeat = "y";
+                                        while (repeat.Equals("y"))
+                                        {
+                                            Console.WriteLine("enter product to purchase");
+                                            string productName = Console.ReadLine();
+                                            Console.WriteLine("enter qty to purchase");
+                                            int qty = byte.Parse(Console.ReadLine());
+
+                                            foreach (Product product in products)
+                                            {
+                                                if (product.pName.Equals(productName))
+                                                {
+                                                    if (product.qtyInStock >= qty)
+                                                    {
+                                                        product.amount = qty * product.price;
+                                                        product.DisplayDetails();
+                                                        cart.Add(product);
+                                                    }
+                                                    else
+                                                    {
+                                                        Console.WriteLine("Stock not available");
+                                                    }
+                                                }
+                                            }
+                                            Console.WriteLine("Purchase more?");
+                                            repeat = Console.ReadLine();
+                                        }
+                                        break;
+                                    }
+                                case 2:
+                                    {
+                                        int totalAmount=0;
+                                        foreach(Product product in cart)
+                                        {
+                                            product.DisplayDetails();
+                                            totalAmount += product.amount; 
+
+                                        }
+                                        Console.WriteLine("TOTAL AMOUNT IS " + totalAmount);    
+                                        break;
+                                    }
+                            }
+                            break;
+                        }
+                        Console.WriteLine("Repeat");
+                        choice = Console.ReadLine();
+                }
+
+            }
+            static int AdminMenu()
+            {
+                Console.WriteLine("Add Product");
+                Console.WriteLine("Display Products");
+                int ch = byte.Parse(Console.ReadLine());
+                return ch;
+            }
+            static int CustomerMenu()
+            {
+                Console.WriteLine("Purchase Product");
+                Console.WriteLine("Produce Bill");
+                int ch = byte.Parse(Console.ReadLine());
+                return ch;
+
+            }
+        }
+
+        }
+
+}
